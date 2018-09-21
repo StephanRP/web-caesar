d813bcebc760ac6a436a2e29c9287b8e2dc2ef84
Stephan <stephanrpeterson@gmail.com>
2018-09-21 15:49:16 -0500

    in textarea

--- flask-env/main.py
+++ flask-env/main.py
@@ -4,25 +4,27 @@ from caesar import rotate_string
 app = Flask(__name__)
 app.config["DEBBUG"] = True
 
+empty=""
+
 form = """
 <!DOCTYPE html>
 
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background=color: #eee;
                 padding 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -32,7 +34,9 @@ form = """
                     <input type="text" name="rot" value="0" />
                 </label>
             </div>    
-            <textarea type="text" name="text"></textarea>
+            <textarea type="text" name="text">
+                {0}
+            </textarea>
             <br>
             <input type="submit" value="Submit Query" />
         </form>
@@ -40,18 +44,21 @@ form = """
 </html>
 """
 
+
+
+
 @app.route("/", methods=["POST"])
 def encrypt():
     rot = request.form["rot"]
     rot = int(rot)
     text = request.form["text"]
     encrypted = rotate_string(text, rot)
-    content = form+"<h1>"+encrypted+"</h1>"
+    content = form.format(encrypted)
     
     return content
 
 @app.route("/")
 def index():
-    return form
+    return form.format(empty)
 
 app.run()
